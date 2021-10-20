04c3c84e5d474849963b602a4f72689f5a6bc07e
Ugo <ugo.prenat@gmail.com>
2021-10-20 10:45:37 +0200

    create user system

--- app.py
+++ app.py
@@ -7,30 +7,138 @@ import json
 
 app = Flask(__name__)
 
-username = 'ugo'
-password = 'XMGYXZTCk4HnhhEj'
+
+# Connection to the data base
+username = 'maxime_ugo'
+password = 'dspDsTMgE5bu0J72'
 
 mongo_uri = f'mongodb+srv://{username}:{password}@cluster0.aiyk2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 client = MongoClient(mongo_uri)
 db = client.g4g
+
+# Collections initiation
 users = db.users
+tournaments = db.tournaments
+games = db.games
 
-def get_users():
-    users_list = users.find()
-    print(users_list)
-    """ for x in users_list:
-        print(x) """
-    return users_list
-
-@app.route("/users", methods=["GET"])
-def users_list():
+def error_msg(code, msg):
     """
-        Apply all request args on the users list and return it
+        Return an error object
+
+        params:
+            code: the code of the error
+            msg: the message of the error
     """
 
-    users = get_users()
-    return 'a'
+    error = {
+        'code': code,
+        'msg': msg
+    }
+    return error
+
+def success_msg(code, msg):
+    """
+        Return an success object
+
+        params:
+            code: the code of the success
+            msg: the message of the success
+    """
+
+    succes = {
+        'code': code,
+        'msg': msg
+    }
+    return succes
+
+def prevent_create_user_errors(args):
+    """
+        Prevent all possible errors
+
+        params:
+            args: array of all arguments passed in the request
+        
+        return the 'error' object
+    """
+
+    error = {
+        'found_error': False,
+        'error_content': {}
+    }
+
+    username = args.get('username')
+    mail = args.get('mail')
+    password = args.get('password')
+    is_admin = args.get('is_admin')
+
+    # Check if required args are missing
+    if username == None:
+        error['found_error'] = True
+        error['error_content'] = error_msg(400, 'L\'argument \'username\' est manquant')
+        return error
+    elif mail == None:
+        error['found_error'] = True
+        error['error_content'] = error_msg(400, 'L\'argument \'mail\' est manquant')
+        return error
+    elif password == None:
+        error['found_error'] = True
+        error['error_content'] = error_msg(400, 'L\'argument \'password\' est manquant')
+        return error
+    elif is_admin == None:
+        error['found_error'] = True
+        error['error_content'] = error_msg(400, 'L\'argument \'is_admin\' est manquant')
+        return error
+
+    # Check if the 'is_admin' value is a boolean
+    if is_admin.lower() != 'false' and is_admin.lower() != 'true':
+        error['found_error'] = True
+        error['error_content'] = error_msg(400, 'La valeur de l\'argument \'is_admin\' est incorrecte, booléen requis')
+        return error
+
+    # Loop on all args to see if an arg is unknown
+    for arg in args:
+        if arg != 'username' and arg != 'mail' and arg != 'password' and arg != 'is_admin' :
+            error['found_error'] = True
+            error['error_content'] = error_msg(400, f'L\'argument \'{arg}\' est inconnu')
+            return error
+
+    # if no error was found, return the 'error' object with the param 'found_error' set to false by default
+    return error
+
+def create_user(args):
+    """
+        Create an object containing the values of the new user and insert it in the database
+
+        params:
+            args: array of all arguments passed in the request
+    """
+
+    new_user = {
+        'username': args.get('username'),
+        'mail': args.get('mail'),
+        'password': args.get('password'),
+        'is_admin': args.get('is_admin'),
+    }
+    users.insert_one(new_user)
+
+
+@app.route("/users", methods=["POST"])
+def create_new_user():
+    """
+        Create a new user with values passed in request args
+
+    """
+    args = request.args
+
+    error = prevent_create_user_errors(args)
+
+    if error['found_error']:
+        return error['error_content']
+
+    else:
+        create_user(args)
+        return success_msg(200, 'L\'utilisateur a été créé')
 
                     
 
